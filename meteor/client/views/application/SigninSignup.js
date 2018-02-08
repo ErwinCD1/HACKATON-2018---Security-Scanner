20c2aa347cb2883eee7825ce1f12290e4f38e5d4
admin <e.charles-dufant@logsystem.fr>
2018-02-08 15:55:20 +0100

    MODIF AJAX LOGIN / LOGOUT

--- meteor/client/views/application/SigninSignup.js
+++ meteor/client/views/application/SigninSignup.js
@@ -1,40 +1,77 @@
-Template.SigninSignup.events({
-    'click #reg_submit_btn':function(){
-        var reg_email_txt  = $('#reg_email_txt').val() ;
-        var reg_name_txt = $('#reg_name_txt').val() ;
-        var reg_password_txt = $('#reg_password_txt').val() ;
-        var reg_password_confirm_txt = $('#reg_password_confirm_txt').val() ;
-
-        // alert(reg_email_txt+'/'+reg_name_txt+'/'+reg_password_txt+'/'+reg_password_confirm_txt);
-
-        HTTP.call( 'POST', 'http://192.168.1.200:53600/api/register', {
-                data: {'name': reg_name_txt,'email': reg_email_txt,'password': reg_password_txt,'password_confirmation': reg_password_confirm_txt
+var cpt = 0;
+function myTokenFunction(string,string){
+        console.log(token_user);
+        if(token_user != "" || token_user != null){
+                try{
+                        window.localStorage.setItem("Content_Token_User"+cpt,token_user);
                 }
-        }, function( error, response ) {
-        if ( error ) {
-                console.log( error );
-        } else {
-                console.log( response.content );
-                alert( response );
+                catch(err){
+                        localStorageAvailable = false;
+                }
         }
-        });
-    },
+}
 
-    'click #log_submit_btn':function(){
-        var log_email_txt  = $('#log_email_txt').val() ;
-        var log_password_txt = $('#log_password_txt').val() ;
-
-        //  alert(log_email_txt+'/'+log_password_txt);
-
-        HTTP.call( 'POST', 'http://192.168.1.200:53600/api/login', {
-            data: { email: log_email_txt,  password: log_password_txt}
-        }, function( error, response ) {
-        if ( error ) {
-                console.log( error );
-        } else {
-                console.log( response.content );
-                alert( response );
+function myRemoveTokenFunction(string,string){
+        try{
+                window.localStorage.removeItem("Content_Token_User"+cpt,token_user);
         }
-        });
-    }
+        catch(err){
+                localStorageAvailable = false;
+        }
+}
+
+Template.SigninSignup.events({
+        'click #reg_submit_btn':function(){
+                var reg_email_txt  = $('#reg_email_txt').val() ;
+                var reg_name_txt = $('#reg_name_txt').val() ;
+                var reg_password_txt = $('#reg_password_txt').val() ;
+                var reg_password_confirm_txt = $('#reg_password_confirm_txt').val() ;
+
+                var token_user = "";
+
+
+                HTTP.call( 'POST', 'http://192.168.1.200:53600/api/register', {
+                        data: {'name': reg_name_txt,'email': reg_email_txt,'password': reg_password_txt,'password_confirmation': reg_password_confirm_txt
+                        }
+                }, function( error, response ) {
+                if ( error ) {
+                        console.log( error );
+                } else {
+                        // console.log( response.data.access_token );
+                        token_user = response.data.access_token;
+                        cpt += 1;
+                        myTokenFunction(cpt,token_user);
+                        document.getElementById('link_logout').style.display = "block";
+                }
+                });
+        },
+
+        'click #log_submit_btn':function(){
+                var log_email_txt  = $('#log_email_txt').val() ;
+                var log_password_txt = $('#log_password_txt').val() ;
+
+                HTTP.call( 'POST', 'http://192.168.1.200:53600/api/login', {
+                data: { email: log_email_txt,  password: log_password_txt}
+                }, function( error, response ) {
+                if ( error ) {
+                        console.log( error );
+                } else {
+                        // console.log( response.data.access_token );
+                        token_user = response.data.access_token;
+                        cpt += 1;
+                        // alert(cpt);
+                        myTokenFunction(cpt,token_user);
+                        document.getElementById('link_logout').style.display = "block";        
+                }
+                });
+        },
+        
+        'click #link_logout':function(){
+                alert('fbsfdgsfgs');
+                // myRemoveTokenFunction(cpt,token_user);
+        },
+
+
+
+    
 });
